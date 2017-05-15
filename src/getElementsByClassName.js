eac2b5b7e0c89129209d7dab4c347a1a05fc31de
Hack Reactor Student <student@hackreactor.com>
2017-05-15 15:41:41 -0700

    Implement getElementByClassName, fix spec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,30 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // declare a result variable of type array
+  let result = [];
+  // helper function takes a node parameter --declare
+  let intoEachNode = node => {
+  //  check if the nodes classlist has any classes
+    if (node.classList) {
+      // cycle through classname looking for target 
+      if (node.classList.contains(className)) {
+        // if found add element to result  
+        result.push(node);
+      }
+    }
+    // check if node has any children
+    if (node.childNodes) {
+      //  if yes  
+      node.childNodes.forEach(child => {
+        // call function and pass children
+        intoEachNode(child);
+      });
+    }
+  };
+    
+  // invoke the helper function on the document body
+  intoEachNode(document.body);
+
+  return result;
 };
